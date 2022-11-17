57e6a7e03ea83ca9bc619f5bda2c408dbfa3506c
mohitbajaj-zs <mohit.bajaj@zopsmart.com>
2022-11-17 18:00:48 +0530

    First commit

---
+++ calculator.go
@@ -0,0 +1,48 @@
+package Calculator
+
+import (
+	"errors"
+	"fmt"
+)
+
+func calculator(a, b float64, ch string) (float64,error) {
+	return 0.0,nil
+}
+func add(a, b float64) float64{
+	return a+b
+}
+func subtract(a, b float64)float64{
+
+		return a-b
+
+}
+
+func multiply(a, b float64)float64{
+
+	return a*b
+}
+
+func division(a, b float64)(float64,error){
+
+	if b!=0{
+	return a/b,nil
+}
+	return 0,errors.New("divide by zero error")
+
+}
+
+func modulo(a, b int)(int,error){
+
+	if b!=0{
+		return a%b,nil
+	}
+	return 0,errors.New("divide by zero error")
+
+}
+
+func main(){
+	a:=5.0
+	b:=6.0
+	ch:="+"
+	fmt.Println(calculator(a,b,ch))
+}
